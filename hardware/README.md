33e35af78fa417e65f0cf4439d98afed59c73090
Niel Nielsen <nieldk@gmail.com>
2018-12-14 09:46:07 +0100

    Add files via upload

---
+++ hardware/README.md
@@ -0,0 +1,5 @@
+Fixes
+=====
+
+- Schematic shows a 2k resistor. Use 1.5k instead.
+- See [the documentation](https://github.com/phikshun/uDuck/tree/master/doc) for suggested parts.
---
+++ src/build/dep/usbdrvasm.o.d
@@ -0,0 +1,40 @@
+usbdrvasm.o: ../usbdrv/usbdrvasm.S ../usbdrv/iarcompat.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/iotn85.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/iotnx5.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h \
+  ../usbdrv/usbdrv.h .././usbconfig.h ../usbdrv/usbdrvasm165.inc \
+  ../usbdrv/asmcommon.inc
+
+../usbdrv/iarcompat.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/iotn85.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/iotnx5.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h:
+
+../usbdrv/usbdrv.h:
+
+.././usbconfig.h:
+
+../usbdrv/usbdrvasm165.inc:
+
+../usbdrv/asmcommon.inc:
